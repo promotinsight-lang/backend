8ebe67e13a026b9c37324db92e7a059633837bc8
Jaman <witchbella17@gmail.com>
2026-05-25 04:31:42 +0600

    giit add

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const pool = require("./config/db");
 require("dotenv").config();
 
 const app = express();
+app.set('trust proxy', 1);
 
 // ==========================================
 // 📡 CREATE HTTP SERVER & INIT SOCKET.IO
