b745ef5e2fe1ad841dbe9940e8c918b25b76be47
Jaman <witchbella17@gmail.com>
2026-07-18 09:40:53 -0700

    Include product link in buyer applications

--- src/controllers/applicationController.js
+++ src/controllers/applicationController.js
@@ -354,7 +354,7 @@ const getMyApplications = async (req, res) => {
          a.review_submitted_at,
          a.review_screenshot_url, a.review_screenshot_url_2, a.review_link, a.refund_screenshot_url, a.refund_comment, a.created_at AS applied_on,
          a.seller_payment_transaction_id, a.seller_payment_screenshot_url, a.seller_payment_note, a.seller_paid_at,
-         p.id AS product_id, p.product_name, p.image_url, p.price, p.reward, p.country, p.platform, p.store_name, p.search_keyword, p.instructions, p.category
+         p.id AS product_id, p.product_name, p.image_url, p.price, p.reward, p.country, p.platform, p.store_name, p.search_keyword, p.instructions, p.product_link, p.category
        FROM applications a JOIN products p ON a.product_id = p.id
        WHERE a.user_id = $1 ORDER BY a.created_at DESC`,
       [userId]
